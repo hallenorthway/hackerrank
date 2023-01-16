8e9b1ce3e954d2030d0ab592e2a3dccc3156654e
hallenorthway <114271620+hallenorthway@users.noreply.github.com>
2023-01-15 22:13:53 -0600

    Create plus minus.java

---
+++ algorithms/plus minus.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        int pos = 0;
+        int neg = 0;
+        int zero = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) > 0) {
+                pos++;
+            } else if (arr.get(i) < 0) {
+                neg++;
+            } else if (arr.get(i) == 0) {
+                zero++;
+            }
+        }
+        System.out.println(pos / (double)arr.size());
+        System.out.println(neg / (double)arr.size());
+        System.out.print(zero / (double)arr.size());
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
